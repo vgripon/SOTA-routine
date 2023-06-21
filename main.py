522bd29272401518ba572435940df950509a10e6
Vincent Gripon <vincent@gripon.fr>
2023-06-21 10:51:36 +0200

    refactoring

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ for era in range(1 if args.adam else 0, args.eras + 1):
             lr = scheduler.get_last_lr()[0]
             accelerator.print("\r{:6.2f}% loss:{:.4e} lr:{:.3e}".format(100 * step / total_steps_for_era, torch.mean(torch.tensor(train_losses)).item(), lr), end="")
 
-            step_time = (time.time() - start_time) / (args.steps * (era - 1 if era > 0 else 0) + step + (5 * len(train_loader) if not args.adam else 0))
+            step_time = (time.time() - start_time) / (args.steps * (era - 1 if era > 0 else 0) + step + (5 * len(train_loader) if not args.adam and era > 0 else 0))
             remaining_time = (total_steps_for_era - step + (args.eras - era - 1) * args.steps) * step_time
             
             if accelerator.is_main_process:
